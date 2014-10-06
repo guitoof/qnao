542cc09d9fb9611c8328c23148f23ce5350cb701
etiennedemontalivet <etienne.demontalivet@gmail.com>
2014-10-06 13:43:39 +0200

    reward class for nao project

---
+++ python/src/reward.py
@@ -0,0 +1,18 @@
+// import nao api
+
+
+class Reward:
+	'reward class for NAO'
+
+	// local Variables
+
+
+	//Constructor 
+	def __init__():
+
+
+
+	// methods
+	def getReward():
+
+	def setReward():
