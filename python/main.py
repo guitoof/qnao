73565edcbbfc13a0c042eee9ede9054e565bca02
Guillaume Diallo-Mulliez <guillaume.diallo@ensta-paristech.fr>
2014-10-13 13:42:03 +0200

    Implementing reward functions:robot now gets positive reward on front touch and negative reward on rear touch

--- python/main.py
+++ python/main.py
@@ -1,13 +1,18 @@
 #!/usr/bin python
 
-ip = "169.254.51.192"
+ip = "169.254.204.42"
+NAO_IP = "nao.local"
 port = 9559
 
 from naoqi import ALProxy
 
 from ArmController import ArmController
-from Reward import RewardModule
+from Reward import Reward
 
+from naoqi import ALBroker
+from optparse import OptionParser
+
+import sys
 import time
 
 
@@ -19,17 +24,49 @@ def stand():
         print "Could not create proxy to ALRobotPosture"
         print "Error was: ", e
 
-    postureProxy.goToPosture("StandInit", 0.2)
+    postureProxy.goToPosture("Sit", 0.5)
 
 ####### Init proxies #######
 
 # Make robot stand if not standing
 #stand()
 
+#armController = ArmController(ip, port)
+#time.sleep(2)
 
-armController = ArmController(ip, port)
+parser = OptionParser()
+parser.add_option( "--pip", help="Parent broker port. The IP Address or your robot", dest="pip" )
+parser.add_option( "--pport", help="Parent broker port. The port NAOqi is listening to", dest="pport", type="int" )
+parser.set_defaults( pip=ip, pport=port )
 
-time.sleep(1)
 
-armController.move('down')
+(opts, args_) = parser.parse_args()
+pip = opts.pip
+pport = opts.pport
+
+myBroker = ALBroker( "myBroker",
+                     "0.0.0.0", # Listen to anyone
+                     0,         # Find a free port and use it
+                     pip,
+                     pport
+                    )
+
+
+rewardModule = Reward("rewardModule")
+goodMemory = ALProxy("ALMemory")
+goodMemory.subscribeToEvent( "FrontTactilTouched", "rewardModule", "onFrontTactilTouched" )
+badMemory = ALProxy("ALMemory")
+badMemory.subscribeToEvent( "RearTactilTouched", "rewardModule", "onRearTactilTouched" )
+
+
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    print
+    print "Interrupted by user. Shutting down ..."
+    myBroker.shutdown()
+    sys.exit(0)
+
+#armController.move('down')
 
--- python/reward.py
+++ python/reward.py
@@ -9,9 +9,34 @@ from naoqi import ALBroker
 from naoqi import ALModule
 
 # Global variable to store the HumanGreeter module instance
-memory = None
+#memory = None
 
-class Reward:
-	"""
-	    Reward class for NAO
-	"""
+class Reward(ALModule):
+
+    value = 0
+
+    def positiveReward(self):
+        print "Receiving congratulation (+1 reward)"
+        self.value += 1
+
+    def negativeReward(self):
+        print "Receiving punishment (-1 reward)"
+        self.value -= 1
+
+    def onFrontTactilTouched(self, *_args):
+        """
+            Callback method for FrontTactilTouched event
+        """
+
+        self.positiveReward()
+        print "Total reward is ", self.value
+        pass
+
+    def onRearTactilTouched(self, *_args):
+        """
+            Callback method for RearTactilTouched event
+        """
+
+        self.negativeReward()
+        print "Total reward is ", self.value
+        pass
