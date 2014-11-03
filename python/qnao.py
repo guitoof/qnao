b2a533e170ce5d5c5d1008813d0800f16285c423
Guillaume Diallo-Mulliez <guillaume.diallo@ensta-paristech.fr>
2014-11-03 18:15:32 +0100

    Speech recognition works. User should say Bravo when the expected position is reached

--- python/qnao.py
+++ python/qnao.py
@@ -14,6 +14,7 @@ from naoqi import ALBroker
 from qlearning import QLearning
 import config
 
+
 reward_module = None
 
 class QLearning(object):
@@ -70,6 +71,7 @@ class QLearning(object):
             self.tts.say("Commençons ainsi")
             time.sleep(1)
 
+            success = False
             while (state != self.goal_state):
                 action = self.policy(state, self.Q[state.position_index()])
                 next_state = np.array(state.value)+action.get_2D_offset()
@@ -80,10 +82,11 @@ class QLearning(object):
 
                 reward_module.subscribe_to_events()
 
-                while (not(reward_module.value)):
+                while (not(reward_module.value) and (reward_module.memory.getData("WordRecognized")[1] < 0.4)):
                     time.sleep(1)
-
-                print reward_module.success_memory.getOutputNames()
+                if (reward_module.memory.getData("WordRecognized")[1] >= 0.4 ):
+                    success = True
+                #print len(reward_module.memory.WordRecognized)
 
                 current_reward = reward_module.value
                 if current_reward == 1:
@@ -98,9 +101,10 @@ class QLearning(object):
                 state = next_state
                 print "Now at %s position (goal %s)" % (state.name, self.goal_state.name)
             print ""
-            self.tts.say("Incroyable, j'ai réussi on dirait")
-        print self.Q
-        self.tts.say("On a terminé tout le monde. Regarde la matrice Q que j'ai craché à l'écran")
+            if success :
+                self.tts.say("Incroyable, j'ai réussi on dirait")
+                print self.Q
+                self.tts.say("On a terminé tout le monde. Regarde la matrice Q que j'ai craché à l'écran")
 
     def show_results(self):
         states = list(State)
@@ -139,6 +143,6 @@ args = parser.parse_args(namespace=qlearning)
 reward_module = Reward("reward_module", "nao_broker", args.naoIP, args.naoPort)
 
 qlearning.init_experiment()
-qlearning.launch_experiment()
+#qlearning.launch_experiment()
 #qlearning.show_results()
 qlearning.end_experiment()
